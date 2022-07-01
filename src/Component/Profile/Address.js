25f2af5f7a8b4917536a7ef1ea762929d6ce4bd2
Tinhinane Seddik <tinhinaneseddik7@gmail.com>
2022-07-01 17:44:28 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Address() {
+  return (
+    <div className="Address">
+      <span>E-Mail: tinhinaneseddik7@gmail.com</span>
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FullName() {
+  return (
+    <div className="Name">
+      <h1> Tinhinane Seddik </h1>
+    </div>
+  );
+}
+
+export default FullName;
